a83531063ac14af1eecaa022bc2c0baa937bbb54
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-11 11:27:22 +0500

    this is the 29th project from 50 project

--- src/App.js
+++ src/App.js
@@ -1,76 +1,53 @@
 import './App.css';
-import React,{useEffect,useState} from 'react';
-import axios from 'axios';
-
+import React,{useState,useRef} from 'react';
 
 
 function App() {
-  
-  const [state, setState] = useState({
-    value: '',
-    show: ''
-  });
-  const [data,setData] =useState()
-  const [repos,setRepos] =useState()
-
-  useEffect(()=>{
-    const getData = async (name) => {
-      const res = await axios.get(`https://api.github.com/users/${name}`)
-      const res2 =await axios.get(`https://api.github.com/users/${name}/repos`);
-      console.log(res2.data)
-      setRepos(res2.data);
-      setData(res.data);
-    }
-    getData(state.show)
-    console.log(state.show)
-    setState({value:''})
-  },[state.show])
+  const clickMe = useRef(null)
+const time = useRef(null)
+const [click,setClick] = useState(0)
+const [count,setCount] = useState(0)
 
   const handleChange = (e) => {
-    e.preventDefault()
-    setState({ show: state.value })
+    if(click == 0) {
+      setClick(new Date().getTime())
+
+    }else {
+      if((new Date().getTime() - click) < 400){
+       createHeart(e)
+        setClick(0)
+      }else{
+        setClick(new Date().getTime())
+      }
+    }
   }
- 
+  
+  const createHeart = (e) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+
+    const x =e.nativeEvent.clientX;
+    const y = e.nativeEvent.clientY;
+    const leftOffset = e.target.offsetLeft;
+    const topOffset = e.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+    
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+    clickMe.current.appendChild(heart);
+    setCount(count+1);
+    setTimeout(()=>heart.remove(),5000)
+  }
+
   return (
     <>
-        <form  className="user-form" id="form" onSubmit={handleChange}>
-            <input type="text" placeholder="Search a github user"id="search" value={state.value} onChange={(e)=>setState({value:e.target.value})}/>
-      
-        </form>
-        <main id="main">
-          {
-          data ?
-          <div className="card">
-                <div>
-                    <img src={data?.avatar_url} className="avatar" width="200px"  alt="" />
-                </div>
-                <div className="user-info">
-                    <h2>{data?.name || data?.login}</h2>
-                    <p>{data?.bio}</p>
-                    <ul>
-                        <li>{data?.followers} <strong>Followers</strong></li>
-                        <li>{data?.following} <strong>Following</strong></li>
-                        <li>{data?.public_repos} <strong>Repos</strong></li>
-
-                    </ul>
-                    <div id="repo">
-                        {repos && repos.slice(0,10).map((e)=>(
-                            <a href={e.html_url} className="repos" target="_blank">{e.name}</a>
-                        ))
-                          }
-            
-                    </div>
-                </div>
-            </div>
-            :
-            <div className="card" style={{marginLeft:60}}>
-          <h1>No Profile with this username</h1>
-            </div>
-
-        }
-            
-        </main>
-    </>
+    <h3>Double click on the  image to <i className='fas fa-heart'></i> it</h3>
+    <small>You liked it <span id='times' ref={time}>{count}</span>{count > 1 ? " times" : " time"}</small>
+    <div className="loveMe" ref={clickMe} onClick={(e)=>handleChange(e)}></div>
+</>
   )
 }
 
