4d0a1d3df6517a4584b392581d8b5d3fbbc986f1
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-11 14:09:46 +0500

    this is the 32th project from 50 project

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
 import './App.css';
-import React,{useState,useRef,useEffect} from 'react';
+import React,{useState} from 'react';
 
 
 function App() {
-  const text = useRef(null)
-  const speedEl = useRef(null)
-  const [count,setCount] = useState(1)
-  const [line] = useState("We Love Programming!");
-  let idx = 1;
-  
-  let speed = 300;
-  useEffect(()=>{
-    const write = () => {
-      text.current.innerText = line.slice(0,idx);
-    idx++;
-    if(idx > line.length) {
-    idx = 1;
+  const[count,setCount] = useState({
+    count1:false,
+    count2:false,
+    count3:false
+  })
+
+  const handleChange = (e) => {
+    if(e.target.id == "good"){
+      setCount({
+        count1:!count.count1,
+        count2:false,
+        count3:false,
+      })
     }
-    setTimeout(write,speed);
-  
+   else if(e.target.id == "cheap"){
+      setCount({
+        count1:false,
+        count2:!count.count2,
+        count3:false,
+      })
+    }
+    else if(e.target.id == "fast"){
+      setCount({
+        count1:false,
+        count2:false,
+        count3:!count.count3,
+      })
+    }
   }
-  write()
-
-
- speedEl.current.addEventListener("input",(e)=>speed = 300 / e.target.value)
-  
-},[idx])
 
   return (
-    <div className='main'>
-    <h1  ref={text}></h1>
-    <div className='child'>
-        <label htmlFor="speed">Speed: </label>
-        <input type="number" name="speed" ref={speedEl} onChange={(e)=>setCount(e.target.value)} value={count}  min="1" max="5" step="1" />
+    <>
+        <h2>How do you want your project to be?</h2>
+        <div className="toggle-container">
+            <input type="checkbox" name="" id="good" checked={count.count1} onClick={handleChange} className='toggle' />
+            <label htmlFor="good" className='label'>
+                <div className="ball"></div>
+            </label>
+            <span>Good</span>
+        </div>
 
-    </div>
-</div>
+        <div className="toggle-container">
+            <input type="checkbox" checked={count.count2} onClick={handleChange} name="" id="cheap" className='toggle' />
+            <label htmlFor="cheap" className='label'>
+                <div className="ball"></div>
+            </label>
+            <span>Nice</span>
+        </div>
+
+        <div className="toggle-container">
+            <input type="checkbox" name="" id="fast" checked={count.count3} onClick={handleChange} className='toggle' />
+            <label htmlFor="fast" className='label'>
+                <div className="ball"></div>
+            </label>
+            <span>Bad</span>
+        </div>
+    </>
   )
 }
 
