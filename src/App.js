167f69e98b5a3785dd9ada16f046919924be1cd8
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-12 15:41:16 +0500

    this is the 34th project from 50 project

--- src/App.js
+++ src/App.js
@@ -1,140 +1,74 @@
 import './App.css';
-import React,{useState,useRef} from 'react';
+import React,{useRef,useEffect} from 'react';
+
 
 
 function App() {
-
-  const resultEl = useRef(null)
-  const lengthEl = useRef(null)
-  const uppercaseEl = useRef(null)  
-  const lowercaseEl = useRef(null)
-  const numbersEl = useRef(null)
-  const symbolsEl = useRef(null)
-  const generateEl = useRef(null)
-  const [count,setCount] = useState(4)
-
-
-
-  const getRandomLower = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
+  const counter = useRef(null)
+  const finalMessage = useRef(null)
+  const reply = useRef(null)
   
-  }
- 
-
-  const getRandomUpper = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65);
   
-  }
+  useEffect(()=>{
+    const nums = document.querySelectorAll('.nums span')
+    
+    function resetDom() {
+      counter.current.classList.remove('hide')
+      finalMessage.current.classList.remove('show')
 
+      nums.forEach((num)=>{
+        num.classList.value = ''
 
-  const getRandomNumber = () => {
-    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 48);
-  
-  }
-
-
- const getRandomSymbol = () => {
-      const symbol = '!@$^&*(){}[]=<>/,.';
-    return symbol[Math.floor(Math.random() * symbol.length)];
-  
-  }
-
-  const randomFunc = {
-    lower:getRandomLower,
-    upper:getRandomUpper,
-    number:getRandomNumber,
-    symbol:getRandomSymbol
-  }
-
-  const handleChange = () => {
-    const length = +lengthEl.current.value;
-    const hasLower = lowercaseEl.current.checked
-    const hasUpper = uppercaseEl.current.checked
-    const hasNumber = numbersEl.current.checked
-    const hasSymbol = symbolsEl.current.checked
-    resultEl.current.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol,length)
-
-  }
-
-  const generatePassword = (lower,upper,number,symbol,length) => {
-    let generatedPassword = '';
-    const typeCount = lower + upper + number + symbol;
-    const typeArray = [{lower},{upper},{number},{symbol}].filter(item => Object.values(item)[0])
- 
-    if(typeCount == 0) {
-      return "";
-    }
-
-    for(let i = 0 ; i < length ; i+=typeCount){
-      typeArray.forEach(type=>{
-        const funcName = Object.keys(type)[0]
-        generatedPassword += randomFunc[funcName]()
       })
+
+      nums[0].classList.add('in')
     }
-    const finalPassword = generatedPassword.slice(0,length)
-    return finalPassword;
-
+    
+    function runAnimation() {
+    nums.forEach((num,idx) => {
+      const nextToLast = nums.length - 1;
+       num.addEventListener('animationend',(e)=>{
+         if(e.animationName == "goIn" && idx !== nextToLast){
+           num.classList.remove('in')
+           num.classList.add('out')
+          
+         }
+         else if(e.animationName == "goOut" && num.nextElementSibling) {
+            num.nextElementSibling.classList.add('in')
+         
+         }else{
+           counter.current.classList.add('hide')
+           finalMessage.current.classList.add('show')
+          
+          
+         }
+       })
+    })
   }
+  runAnimation()
+  reply.current.addEventListener('click',()=>{
+    resetDom()
+    runAnimation()
 
+  })
+},[])
 
-  const copyPassword = () => {
-    const textarea = document.createElement("textarea")
-    const password = resultEl.current.innerText
-
-    if(!password){
-      return
-    }
-    textarea.value = password
-    document.body.appendChild(textarea)
-    textarea.select()
-    navigator.clipboard.writeText(textarea.value);
-    textarea.remove()
-    alert("Password copied to clipboard");
-  }
-
-  const handleValue = (e) => {
-    if(e.target.value >=4 && e.target.value<20){
-      setCount(e.target.value)
-    }else{
-      setCount(4)
-    }
-  }
 
   return (
     <>
-        <div className="container">
-            <h2>Password Generator</h2>
-            <div className="result-container">
-                <span className='result' ref={resultEl}></span>
-                <button className='btn' id='clipboard' onClick={copyPassword}>
-                    <i className='far fa-clipboard'></i>
-                </button>
+        <div className="counter" ref={counter}>
+            <div className="nums">
+                <span className='in'>3</span>
+                <span>2</span>
+                <span>1</span>
+                <span>0</span>
             </div>
-            <div className="settings">
-                <div className="setting">
-                    <label htmlFor="">password length</label>
-                    <input type="number" min="4" id="length" max="20" value={count} onChange={(e)=>handleValue(e)} ref={lengthEl} />
-                </div>
-                <div className="setting">
-                    <label htmlFor="">Include uppercase letters</label>
-                    <input type="checkbox" name="" id="uppercase"   ref={uppercaseEl}/>
-                </div>
-                <div className="setting">
-                    <label htmlFor="">Include lowercase letters</label>
-                    <input type="checkbox" name="" id="lowercase"  ref={lowercaseEl} />
-                </div>
-                <div className="setting">
-                    <label htmlFor="">Include numbers </label>
-                    <input type="checkbox" name="" id="numbers"  ref={numbersEl} />
-                </div>
-                <div className="setting">
-                    <label htmlFor="">Include symbols</label>
-                    <input type="checkbox" name="" id="symbols"  ref={symbolsEl} />
-                </div>
-            </div>
-            <button className="btn btn-large" id="generate" ref={generateEl} onClick={(e)=>handleChange(e)}>
-                Generate Password
-            </button>
+            <h4>Get Ready</h4>
+        </div>
+        <div className="final" ref={finalMessage}>
+            <h1>Go</h1>
+            <button id="replay" ref={reply}>Replay</button>
+            
         </div>
     </>
   )
