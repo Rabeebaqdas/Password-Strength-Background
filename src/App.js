5fe7b393889abf0b71c0ce326b0086604b6b7e5b
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-12 12:45:43 +0500

    this is the 31th project from 50 project

--- src/App.js
+++ src/App.js
@@ -1,63 +1,140 @@
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState,useRef} from 'react';
 
 
 function App() {
-  const[count,setCount] = useState({
-    count1:false,
-    count2:false,
-    count3:false
-  })
 
-  const handleChange = (e) => {
-    if(e.target.id == "good"){
-      setCount({
-        count1:!count.count1,
-        count2:false,
-        count3:false,
+  const resultEl = useRef(null)
+  const lengthEl = useRef(null)
+  const uppercaseEl = useRef(null)  
+  const lowercaseEl = useRef(null)
+  const numbersEl = useRef(null)
+  const symbolsEl = useRef(null)
+  const generateEl = useRef(null)
+  const [count,setCount] = useState(4)
+
+
+
+  const getRandomLower = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);
+  
+  }
+ 
+
+  const getRandomUpper = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65);
+  
+  }
+
+
+  const getRandomNumber = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 48);
+  
+  }
+
+
+ const getRandomSymbol = () => {
+      const symbol = '!@$^&*(){}[]=<>/,.';
+    return symbol[Math.floor(Math.random() * symbol.length)];
+  
+  }
+
+  const randomFunc = {
+    lower:getRandomLower,
+    upper:getRandomUpper,
+    number:getRandomNumber,
+    symbol:getRandomSymbol
+  }
+
+  const handleChange = () => {
+    const length = +lengthEl.current.value;
+    const hasLower = lowercaseEl.current.checked
+    const hasUpper = uppercaseEl.current.checked
+    const hasNumber = numbersEl.current.checked
+    const hasSymbol = symbolsEl.current.checked
+    resultEl.current.innerText = generatePassword(hasLower,hasUpper,hasNumber,hasSymbol,length)
+
+  }
+
+  const generatePassword = (lower,upper,number,symbol,length) => {
+    let generatedPassword = '';
+    const typeCount = lower + upper + number + symbol;
+    const typeArray = [{lower},{upper},{number},{symbol}].filter(item => Object.values(item)[0])
+ 
+    if(typeCount == 0) {
+      return "";
+    }
+
+    for(let i = 0 ; i < length ; i+=typeCount){
+      typeArray.forEach(type=>{
+        const funcName = Object.keys(type)[0]
+        generatedPassword += randomFunc[funcName]()
       })
     }
-   else if(e.target.id == "cheap"){
-      setCount({
-        count1:false,
-        count2:!count.count2,
-        count3:false,
-      })
+    const finalPassword = generatedPassword.slice(0,length)
+    return finalPassword;
+
+  }
+
+
+  const copyPassword = () => {
+    const textarea = document.createElement("textarea")
+    const password = resultEl.current.innerText
+
+    if(!password){
+      return
     }
-    else if(e.target.id == "fast"){
-      setCount({
-        count1:false,
-        count2:false,
-        count3:!count.count3,
-      })
+    textarea.value = password
+    document.body.appendChild(textarea)
+    textarea.select()
+    navigator.clipboard.writeText(textarea.value);
+    textarea.remove()
+    alert("Password copied to clipboard");
+  }
+
+  const handleValue = (e) => {
+    if(e.target.value >=4 && e.target.value<20){
+      setCount(e.target.value)
+    }else{
+      setCount(4)
     }
   }
 
   return (
     <>
-        <h2>How do you want your project to be?</h2>
-        <div className="toggle-container">
-            <input type="checkbox" name="" id="good" checked={count.count1} onClick={handleChange} className='toggle' />
-            <label htmlFor="good" className='label'>
-                <div className="ball"></div>
-            </label>
-            <span>Good</span>
-        </div>
-
-        <div className="toggle-container">
-            <input type="checkbox" checked={count.count2} onClick={handleChange} name="" id="cheap" className='toggle' />
-            <label htmlFor="cheap" className='label'>
-                <div className="ball"></div>
-            </label>
-            <span>Nice</span>
-        </div>
-
-        <div className="toggle-container">
-            <input type="checkbox" name="" id="fast" checked={count.count3} onClick={handleChange} className='toggle' />
-            <label htmlFor="fast" className='label'>
-                <div className="ball"></div>
-            </label>
-            <span>Bad</span>
+        <div className="container">
+            <h2>Password Generator</h2>
+            <div className="result-container">
+                <span className='result' ref={resultEl}></span>
+                <button className='btn' id='clipboard' onClick={copyPassword}>
+                    <i className='far fa-clipboard'></i>
+                </button>
+            </div>
+            <div className="settings">
+                <div className="setting">
+                    <label htmlFor="">password length</label>
+                    <input type="number" min="4" id="length" max="20" value={count} onChange={(e)=>handleValue(e)} ref={lengthEl} />
+                </div>
+                <div className="setting">
+                    <label htmlFor="">Include uppercase letters</label>
+                    <input type="checkbox" name="" id="uppercase"   ref={uppercaseEl}/>
+                </div>
+                <div className="setting">
+                    <label htmlFor="">Include lowercase letters</label>
+                    <input type="checkbox" name="" id="lowercase"  ref={lowercaseEl} />
+                </div>
+                <div className="setting">
+                    <label htmlFor="">Include numbers </label>
+                    <input type="checkbox" name="" id="numbers"  ref={numbersEl} />
+                </div>
+                <div className="setting">
+                    <label htmlFor="">Include symbols</label>
+                    <input type="checkbox" name="" id="symbols"  ref={symbolsEl} />
+                </div>
+            </div>
+            <button className="btn btn-large" id="generate" ref={generateEl} onClick={(e)=>handleChange(e)}>
+                Generate Password
+            </button>
         </div>
     </>
   )
