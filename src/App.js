2b52f0efd2f49b947f6cb312413314fcb8985667
Rabeebaqdas <MuhammadAdeelUsman>
2022-04-13 15:02:59 +0500

    this is the 36th project from 50 project

--- src/App.js
+++ src/App.js
@@ -4,67 +4,38 @@ import React,{useRef,useEffect} from 'react';
 
 
 function App() {
-  const img = useRef(null)
-  const leftbtn = useRef(null)
-  const rightbtn = useRef(null)
-  
-  
-  let idx = 0;
-  
+  const container = useRef(null)  
+  const color = ['#e74c3c' , "#8e44ad" , "#3498db" , "#e67e22" , "#2ecc71"];
+  const squares = 500;
+
+
+
   useEffect(()=>{
-    const image = document.querySelectorAll('#imgs img')
-      console.log(idx)
-    const run = () => {
-      idx++;
-      changeImage()
-    }
-  
-    const changeImage = () => {
-      if(idx > image.length - 1) {
-    idx = 0;
-  
-  }else if(idx < 0) {
-    idx = image.length - 1;
+    for(let i = 0 ; i < squares ; i++) {
+      const square = document.createElement('div')
+      square.classList.add('square')
+      square.addEventListener('mouseover',() => setColor(square))
+      square.addEventListener('mouseout',() => removeColor(square))
+        container.current.appendChild(square)
   }
-  
-  img.current.style.transform = `translateX(${-idx * 500}px)`
-    }
-  
-  let interval = setInterval(run,2000)
-
-  function reset() {
-    clearInterval(interval)
-    interval = setInterval(run,2000)
+  function setColor(element) {
+    const color = getRandomColor()
+    element.style.background = color;
+    element.style.boxShadow = `0 0 2px ${color},  0 0 10px ${color}`
   }
-
-  rightbtn.current.addEventListener('click',()=>{
-    idx++
-    changeImage()
-    reset()
-  })
-  leftbtn.current.addEventListener('click',()=>{
-    idx--
-    changeImage()
-    reset()
-  })
-  },[idx])
-
+  function removeColor(element) {
+      element.style.background = '#1d1d1d'
+      element.style.boxShadow = '0 0 2px #000'
+  }
+function getRandomColor() {
+  return color[Math.floor(Math.random() * color.length)]
+}
+  },[])
   return (
     <>
-        <div className="carousel">
-            <div className="image-container" id="imgs" ref={img}>
-                <img src={"https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=961&q=80"} alt="" />
-                <img src={"https://images.unsplash.com/photo-1648737154448-ccf0cafae1c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} alt="" />
-                <img src={"https://images.unsplash.com/photo-1649821042125-699d75d0a893?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} alt="" />
-                <img src={"https://images.unsplash.com/photo-1649821755622-d22da86de3b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"} alt="" />
-
-            </div>
-            <div className="button-container">
-                <button id="left" className='btn' ref={leftbtn}>Prev</button>
-                <button id="right" className='btn' ref={rightbtn}>Next</button>
-            </div>
-        </div>
-    </>
+    <div className="container" ref={container}>
+    </div>
+</>
   )
 }
 
